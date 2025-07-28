25328d61bdf4842bdca7ee456b4c3d84690f3efc
tongbora <tongbora.officail@gmail.com>
2025-07-28 22:52:13 +0700

    One Signal Notification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import { Roboto_Mono } from "next/font/google";
-
+import Script from "next/script"; // Import the Script component
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+// import Script from "next/script";
 
 const robotoMono = Roboto_Mono({
   subsets: ["latin"],
@@ -21,8 +22,42 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  console.log(process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID);
+
   return (
     <html lang="en">
+      <head>
+        {/* Register the OneSignal service worker */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            if ('serviceWorker' in navigator) {
+              navigator.serviceWorker.register('/OneSignalSDKWorker.js').then(
+                () => console.log('Service Worker registered'),
+                (error) => console.error('Service Worker registration failed:', error)
+              );
+            }
+          `,
+          }}
+        ></script>
+
+        {/* Load the OneSignal SDK */}
+        <Script
+          src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js"
+          strategy="afterInteractive"
+        />
+        {/* Initialize OneSignal */}
+        <Script id="onesignal-init" strategy="afterInteractive">
+          {`
+            window.OneSignalDeferred = window.OneSignalDeferred || [];
+            OneSignalDeferred.push(function(OneSignal) {
+              OneSignal.init({
+                appId: "${process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID}",
+              });
+            });
+          `}
+        </Script>
+      </head>
       <body className={`${robotoMono.className}  min-h-screen`}>
         <Navbar />
         <main className="">{children}</main>
